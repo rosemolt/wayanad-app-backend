cf228e94205d165f19df9d33b58a5e049bdf259a
rosemolt <rosemolthomas02@gmail.com>
2024-08-10 04:16:13 -0700

    adminSignUp created

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const bcrypt = require("bcrypt")
+const loginModel = require("./admin")
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://rosemol:rosemol1t@cluster0.4vxoeox.mongodb.net/rescuedb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.get("/test",(req,res)=>{
+    res.json({"status":"success"})
+})
+
+app.post("/adminsignup",(req,res)=>{
+    let input = req.body
+    let hashedpassword = bcrypt.hashSync(input.password,10)
+    input.password = hashedpassword
+    console.log(input)
+    let result = new loginModel(input)
+    result.save()
+    res.json({"status":"success"}) 
+})
+
+app.listen(5050,()=>{
+    console.log("server started")
+})
---
+++ models/admin.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+const loginSchema = mongoose.Schema(
+    {
+        name : String,
+        username : String,
+        password : String
+    }
+)
+
+const loginModel = mongoose.model("logindata",loginSchema)
+module.exports = loginModel
